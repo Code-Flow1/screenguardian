736180483fc3a02976de53a089f303b9fd0131ac
Ayush Deep <aayushdeep979@gmail.com>
2025-04-27 13:54:54 +0530

    Delete preload.js

--- preload.js
+++
@@ -1,6 +0,0 @@
-const { contextBridge, ipcRenderer } = require('electron');
-
-contextBridge.exposeInMainWorld('electronAPI', {
-  openBlur: () => ipcRenderer.send('open-blur'),
-  closeBlur: () => ipcRenderer.send('close-blur')
-});
